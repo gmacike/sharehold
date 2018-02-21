d53b142619353f3c5c2ec833e24e6e16a793012e
Macku <macku30@gmail.com>
2018-02-21 20:04:35 +0100

    initail clients handling

--- catalogue/forms.py
+++ catalogue/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from catalogue.models import BoardGameItem
+from catalogue.models import (BoardGameItem, RentalClient)
 
 class BoardGameForm(forms.ModelForm):
 
@@ -20,4 +20,26 @@ class BoardGameForm(forms.ModelForm):
                 'placeholder':'Wczytaj kod paskowy'}),
             'bggURL': forms.URLInput(attrs={'class':'urlinputclass',
                 'placeholder':'Podaj adres strony gry w serwisie boardgamegeek.com'})
+        }
+
+
+class RentalClientForm(forms.ModelForm):
+
+    class Meta():
+        model = RentalClient
+        fields = ('identificationCode', 'initials')
+
+        labels = {
+            'identificationCode' : 'Oznaczenie Identyfikacyjne',
+            'initials' : 'Inicjały',
+        }
+
+        placeholders = {
+            'identificationCode' : 'Wprowadź oznaczenie identyfikacyjne',
+            'initials' : 'Wprowadź inicjały',
+        }
+
+        widgets = {
+            'itemLabel': forms.TextInput(attrs={'class':'textinputclass'}),
+            'codeValue': forms.TextInput(attrs={'class':'textinputclass'})
         }
