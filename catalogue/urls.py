33cbbc647e9283eaae0dc42ea74d16f96510cdfb
gmacike <macike@o2.pl>
2018-02-25 11:23:31 +0100

    rodzielenie towaru (Commodity) od przedmiotu katalogowego (CatalogueItem); umożliwienie dodawania edycji gier planszowych (towarów rozpoznawanych kodem paskowym) i wiązania ich z grami; umożliwienie wskazywania podstawek dla rozszerzeń; zdjęć pudełek dla towarów; rozszerzenie widoku szczegółów gry

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -3,10 +3,13 @@ from catalogue import views
 
 urlpatterns = [
     url(r'^brdgm/$', views.CatalogueItemListView.as_view(), name='catalogue_entries'),
-    url(r'^brdgm/(?P<pk>\d+)$', views.BoardGameDetailsView.as_view(), name='boardgame_detail'),
-    url(r'^brdgm/new/$', views.BoardGameCreateView.as_view(), name='boardgame_new'),
+    url(r'^brdgm/(?P<pk>\d+)$', views.BoardGameItemDetailsView.as_view(), name='boardgame_detail'),
+    url(r'^brdgm/new/$', views.BoardGameItemCreateView.as_view(), name='boardgame_new'),
     url(r'^brdgm/new/repeat_add_boardgame', views.repeat_add_boardgame, name='boardgame_repeat_new'),
     url(r'^brdgm/new/boardgamelist_return', views.boardgamelist_return, name='boardgamelist_return'),
     url(r'^brdgm/new/return_home', views.return_home, name='return_home'),
-    url(r'^brdgm/(?P<pk>\d+)/edit/$', views.BoardGameUpdateView.as_view(), name='boardgame_edit'),
+    url(r'^brdgm/(?P<pk>\d+)/edit/$', views.BoardGameItemUpdateView.as_view(), name='boardgame_edit'),
+    url(r'^brdgmed/new/$', views.BoardGameCommodityCreateView.as_view(), name='boardgame_new_edition'),
+    url(r'^brdgmed/new/(?P<bgpk>\d+)$', views.BoardGameCommodityCreateView.as_view(), name='boardgame_new_edition'),
+    url(r'^brdgmed/new/return_home', views.return_home, name='return_home'),
 ]
