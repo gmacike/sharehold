cdb8cdfc3eb4ef90383bb4d4b94c3ade18e84ff0
wiatrogon <wiatrogon@gmail.com>
2018-03-04 16:02:16 +0100

    minor improvements

--- circulation/models.py
+++ circulation/models.py
@@ -7,6 +7,8 @@ class RentalClient(models.Model):
     identificationCode = models.IntegerField(unique=True)
     initials = models.CharField(max_length=10)
 
+    def __str__(self):
+        return '{} {}'.format(self.initials, self.identificationCode)
     def getIdentificationCode(self):
         return self.identificationCode
 
--- circulation/urls.py
+++ circulation/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     url(r'^rClient/(?P<pk>\d+)/edit/$', views.ClientUpdateView.as_view(), name='rentalclient_edit'),
     url(r'^rClient/new/return_home', views.return_home, name='return_home'),
 
-    url(r'^rental$', views.ClientHasBoardGameList.as_view(), name='clienthasboardgame_list'),
-    url(r'^rental/new$', views.ClientHasBoardGameCreateView.as_view(), name='clienthasboardgame_create')
+    url(r'^rental$',
+        views.ClientHasBoardGameList.as_view(),
+        name='clienthasboardgame_list'),
+    url(r'^rental/new$',
+        views.ClientHasBoardGameCreateView.as_view(success_url='rental'),
+        name='clienthasboardgame_create')
 ]
